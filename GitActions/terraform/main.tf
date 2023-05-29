390a853f5a3f590f3b118621f5841e21b2abb750
aofvalley <alfonsod@microsoft.com>
2023-05-29 16:47:30 +0200

    tf init

---
+++ GitActions/terraform/main.tf
@@ -0,0 +1,66 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfazsqldbRG"
+    storage_account_name = "tfazsqldbsatest01"
+    container_name       = "terraform"
+    key                  = "terraform.tfstate"
+  }
+}
+
+
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = random_pet.rg_name.id
+  location = var.resource_group_location
+  tags     = var.default_tags
+}
+
+resource "random_pet" "azurerm_mssql_server_name" {
+  prefix = "sql"
+}
+
+resource "random_password" "admin_password" {
+  count       = var.admin_password == null ? 1 : 0
+  length      = 20
+  special     = true
+  min_numeric = 1
+  min_upper   = 1
+  min_lower   = 1
+  min_special = 1
+}
+
+locals {
+  admin_password = try(random_password.admin_password[0].result, var.admin_password)
+}
+
+resource "azurerm_mssql_server" "server" {
+  name                         = random_pet.azurerm_mssql_server_name.id
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  administrator_login          = var.admin_username
+  administrator_login_password = local.admin_password
+  version                      = "12.0"
+  # adding tags to the server
+  tags                         = var.default_tags
+}
+
+resource "azurerm_mssql_database" "db" {
+  name      = var.sql_db_name
+  server_id = azurerm_mssql_server.server.id
+  # adding sku and tags to the database
+  sku_name  = var.sku_name
+  tags      = var.default_tags
+}
+
+# Enables the "Allow Access to Azure services" box as described in the API docs
+# https://docs.microsoft.com/en-us/rest/api/sql/firewallrules/createorupdate
+
+resource "azurerm_mssql_firewall_rule" "serverfw" {
+  name                = "allow-azure-services"
+  server_id           = azurerm_mssql_server.server.id
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
---
+++ GitActions/terraform/variables.tf
@@ -0,0 +1,48 @@
+variable "resource_group_location" {
+  type        = string
+  description = "Location for all resources."
+  default     = "westeurope"
+}
+
+variable "resource_group_name_prefix" {
+  type        = string
+  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+  default     = "rg"
+}
+
+variable "sql_db_name" {
+  type        = string
+  description = "The name of the SQL Database."
+  default     = "SampleDB"
+}
+
+variable "admin_username" {
+  type        = string
+  description = "The administrator username of the SQL logical server."
+  default     = "azureadmin"
+}
+
+variable "admin_password" {
+  type        = string
+  description = "The administrator password of the SQL logical server."
+  sensitive   = true
+  default     = null
+}
+
+variable "sku_name" {
+  type        = string
+  description = "The database sku name. For example, S0, S1, S2, P1, P2, P4, P6, P11, P15, PRS1, PRS2, PRS4, PRS6, PRS11, and PRS15."
+  sensitive   = true
+  default     = "S0"
+}
+
+variable "default_tags" {
+  type        = map(string)
+  description = "Default tags for Azure resources."
+
+  default = {
+    demo1  = "iac"
+    demo2  = "tf"
+    demo3  = "azsql"
+  }
+}
